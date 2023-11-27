97310bb524c0e3fddbdc8154b8c2ceaddd98bcc3
i-be-snek <sjzahra@proton.me>
2023-11-27 18:24:37 +0100

    :recycle: Refactor preprocessing script

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,32 +1,26 @@
-from datasets import load_dataset
+from datasets import load_dataset, DatasetDict
 from tag import TagInfo
 from dataclasses import dataclass
-
-dataset = load_dataset("Babelscape/multinerd")
 
 
 @dataclass
 class Data:
-    data_split = ["test", "validation", "train"]
+    data_split: tuple = ("test", "validation", "train")
 
     @staticmethod
-    def filter_out_tags(example, tags_to_keep: list) -> dict:
+    def filter_out_tags(example: dict, tags_to_keep: list) -> dict:
         ner_tags: list = example["ner_tags"]
         example["ner_tags"] = [
             0 if tag not in tags_to_keep else tag for tag in ner_tags
         ]
         return example
 
-    def __post_init__(self):
-        self.dataset = load_dataset("Babelscape/multinerd")
+    def __post_init__(self) -> None:
+        self.dataset: DatasetDict = load_dataset("Babelscape/multinerd")
 
         # filter out non-English examples
         for ds in self.data_split:
             self.dataset[ds] = self.dataset[ds].filter(lambda x: x["lang"] == "en")
-
-        print(self.dataset["train"][0])
-
-        for ds in self.data_split:
             self.dataset[ds] = self.dataset[ds].remove_columns("lang")
 
         # initiate datasets for each system
